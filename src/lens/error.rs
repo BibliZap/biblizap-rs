40f6a32c853db6a5cae96d03af84ec2f95e8b080
Raphaël Bentégeac <raphael@bentegeac.com>
2023-10-21 09:29:01 +0200

    fix: make error transparent

--- src/lens/error.rs
+++ src/lens/error.rs
@@ -3,15 +3,15 @@ use reqwest::header::ToStrError;
 
 #[derive(Error, Debug)]
 pub enum LensError {
-    #[error("request error")]
+    #[error(transparent)]
     Request(#[from] reqwest::Error),
-    #[error("parse int error")]
+    #[error(transparent)]
     ParseInt(#[from] std::num::ParseIntError),
     #[error("rate limit missing")]
     RateLimitMissing,
-    #[error("request to str error")]
+    #[error(transparent)]
     RequestToStr(#[from] ToStrError),
-    #[error("serde_json error")]
+    #[error(transparent)]
     SerdeJson(#[from] serde_json::Error),
     #[error("value as_str error")]
     ValueAsStr,
